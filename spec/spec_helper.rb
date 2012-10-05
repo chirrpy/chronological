d2c19cb7b340b426d317f7ef5a1e1b917ea1bc41
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-05 16:32:23 -0500

    Install ActiveRecord and SQLite3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,29 @@
+require 'sqlite3'
+require 'active_record'
 require 'chronological'
 
 RSpec.configure do |config|
+  config.before(:all) do
+    SQLite3::Database.new 'tmp/test.db'
+
+    ActiveRecord::Base.establish_connection(
+      :adapter  => 'sqlite3',
+      :database => 'tmp/test.db'
+    )
+
+    class SetupTests < ActiveRecord::Migration
+      def up
+        create_table :chronologicables do |t|
+          t.datetime :started_at_utc
+          t.datetime :ended_at_utc
+        end
+      end
+    end
+
+    SetupTests.new.migrate(:up)
+  end
+
+  config.after(:all) do
+    `rm -f ./tmp/test.db`
+  end
 end
