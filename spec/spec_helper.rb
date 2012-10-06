10326c2b6d29f7e7e7c30165fa706346c6d19b7e
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-05 21:24:07 -0500

    Add common spec support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require 'active_record'
 require 'timecop'
 require 'chronological'
 
+Dir[File.expand_path('../support/**/*.rb',   __FILE__)].each { |f| require f }
+
 RSpec.configure do |config|
   config.before(:all) do
     SQLite3::Database.new 'tmp/test.db'
