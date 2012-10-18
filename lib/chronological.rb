5874565a44d5d4a31f224450114cd1b1fcac7d29
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-17 20:35:53 -0500

    Rename 'start_field' to 'start_time_field' and 'end_field' to 'end_time_field'

--- lib/chronological.rb
+++ lib/chronological.rb
@@ -1,30 +1,30 @@
 module Chronological
   module ClassMethods
     def chronological(options = {})
-      start_field            = options[:start_utc] || options[:start]
-      end_field              = options[:end_utc]   || options[:end]
-      time_zone              = options[:time_zone]
-      start_field_is_utc     = options.has_key? :start_utc
-      end_field_is_utc       = options.has_key? :end_utc
-      start_field_utc_suffix = start_field_is_utc ? '_utc' : ''
-      end_field_utc_suffix   = end_field_is_utc ? '_utc' : ''
+      start_time_field            = options[:start_utc] || options[:start]
+      end_time_field              = options[:end_utc]   || options[:end]
+      time_zone                   = options[:time_zone]
+      start_time_field_is_utc     = options.has_key? :start_utc
+      end_time_field_is_utc       = options.has_key? :end_utc
+      start_time_field_utc_suffix = start_time_field_is_utc ? '_utc' : ''
+      end_time_field_utc_suffix   = end_time_field_is_utc ? '_utc' : ''
 
       define_method(:started_at_utc_date) do
-        return nil unless send(start_field).respond_to? :to_date
+        return nil unless send(start_time_field).respond_to? :to_date
 
-        send(start_field).to_date
+        send(start_time_field).to_date
       end
 
       define_method(:ended_at_utc_date) do
-        return nil unless send(end_field).respond_to? :to_date
+        return nil unless send(end_time_field).respond_to? :to_date
 
-        send(end_field).to_date
+        send(end_time_field).to_date
       end
 
       define_method(:in_progress?) do
         return false unless scheduled?
 
-        (send(start_field) <= Time.now.utc) && send(end_field).future?
+        (send(start_time_field) <= Time.now.utc) && send(end_time_field).future?
       end
 
       define_method(:inactive?) do
@@ -34,13 +34,13 @@ module Chronological
       define_method(:scheduled?) do
         optional_time_zone = !options[:time_zone].nil? ? send(time_zone) : true
 
-        send(start_field).present? && send(end_field).present? && optional_time_zone
+        send(start_time_field).present? && send(end_time_field).present? && optional_time_zone
       end
 
       define_method(:partially_scheduled?) do
         optional_time_zone = !options[:time_zone].nil? ? send(time_zone) : false
 
-        send(start_field).present? || send(end_field).present? || optional_time_zone
+        send(start_time_field).present? || send(end_time_field).present? || optional_time_zone
       end
 
       define_method(:duration) do
@@ -55,27 +55,27 @@ module Chronological
       # Scopes
       #
       self.class.send(:define_method, :by_date) do
-        order "#{table_name}.#{start_field} ASC, #{table_name}.#{end_field} ASC"
+        order "#{table_name}.#{start_time_field} ASC, #{table_name}.#{end_time_field} ASC"
       end
 
       self.class.send(:define_method, :by_date_reversed) do
-        order "#{table_name}.#{start_field} DESC, #{table_name}.#{end_field} DESC"
+        order "#{table_name}.#{start_time_field} DESC, #{table_name}.#{end_time_field} DESC"
       end
 
       self.class.send(:define_method, :expired) do
-        where("#{end_field} <= :now", :now => Time.now.utc)
+        where("#{end_time_field} <= :now", :now => Time.now.utc)
       end
 
       self.class.send(:define_method, :current) do
-        where("#{end_field} > :now", :now => Time.now.utc)
+        where("#{end_time_field} > :now", :now => Time.now.utc)
       end
 
       self.class.send(:define_method, :in_progress) do
-        where("#{start_field} <= :now AND #{end_field} > :now", :now => Time.now.utc)
+        where("#{start_time_field} <= :now AND #{end_time_field} > :now", :now => Time.now.utc)
       end
 
       self.class.send(:define_method, :started) do
-        where("#{start_field} <= :now", :now => Time.now.utc)
+        where("#{start_time_field} <= :now", :now => Time.now.utc)
       end
 
       self.class.send(:define_method, :in_progress?) do
@@ -92,17 +92,17 @@ module Chronological
       #
       # Aliasing date methods to make code more readable
       class_eval do
-        alias_attribute   :"starts_at#{start_field_utc_suffix}",    start_field.to_sym
-        alias_attribute   :"starting_at#{start_field_utc_suffix}",  start_field.to_sym
-        alias_attribute   :"ends_at#{start_field_utc_suffix}",      end_field.to_sym
-        alias_attribute   :"ending_at#{start_field_utc_suffix}",    end_field.to_sym
+        alias_attribute   :"starts_at#{start_time_field_utc_suffix}",    start_time_field.to_sym
+        alias_attribute   :"starting_at#{start_time_field_utc_suffix}",  start_time_field.to_sym
+        alias_attribute   :"ends_at#{start_time_field_utc_suffix}",      end_time_field.to_sym
+        alias_attribute   :"ending_at#{start_time_field_utc_suffix}",    end_time_field.to_sym
 
         alias             active?                                   in_progress?
       end
 
     private
       define_method(:duration_in_seconds) do
-        (send(end_field) - send(start_field))
+        (send(end_time_field) - send(start_time_field))
       end
     end
   end
