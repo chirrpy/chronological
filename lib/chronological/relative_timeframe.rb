8f6f0e62a8477346f5e09622728fa623b1e09796
Mark McEahern <mark@mceahern.com>
2012-10-26 09:19:01 -0500

    Fix the method of joining the prefix and suffix so that when there is no suffix, we don't have a trailing underscore
    This is what was happening:
    
    ['foo', nil].join('_') => 'foo_'

--- lib/chronological/relative_timeframe.rb
+++ lib/chronological/relative_timeframe.rb
@@ -6,10 +6,10 @@ module Chronological
         base_time_field_is_utc = options.has_key? :base_utc
         time_field_utc_suffix  = base_time_field_is_utc ? 'utc' : nil
 
-        start_time_field       = ['started_at', time_field_utc_suffix].join('_')
-        end_time_field         = ['ended_at',   time_field_utc_suffix].join('_')
-        start_date_field       = ['started_on', time_field_utc_suffix].join('_')
-        end_date_field         = ['ended_on',   time_field_utc_suffix].join('_')
+        start_time_field       = ['started_at', time_field_utc_suffix].compact.join('_')
+        end_time_field         = ['ended_at',   time_field_utc_suffix].compact.join('_')
+        start_date_field       = ['started_on', time_field_utc_suffix].compact.join('_')
+        end_date_field         = ['ended_on',   time_field_utc_suffix].compact.join('_')
 
         define_method(start_time_field) do
           return nil unless send(base_time_field).present? && send(options[:start]).present?
