988d8aa4d9f5c9eda4c7e94a6eadd6c0f8baf1bc
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-28 15:19:16 -0500

    Whitespace

--- spec/strategy_resolver_spec.rb
+++ spec/strategy_resolver_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Chronological::StrategyResolver do
   describe '.resolve' do
-  context 'when passed a strategy name' do
-    it 'resolves to the proper class' do
-      Chronological::StrategyResolver.resolve(:type => :absolute).should be_an Chronological::AbsoluteStrategy
+    context 'when passed a strategy name' do
+      it 'resolves to the proper class' do
+        Chronological::StrategyResolver.resolve(:type => :absolute).should be_an Chronological::AbsoluteStrategy
+      end
     end
-  end
   end
 end
