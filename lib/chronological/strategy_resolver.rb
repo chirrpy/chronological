cd78ffb3d88b852639553f9a80023efe74149bd8
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-28 17:57:44 -0500

    Break out the constants in the StrategyResolver so that the DEFAULT_FIELD_NAMES and the VALID_OPTIONS can be accessed separately as well as together

--- lib/chronological/strategy_resolver.rb
+++ lib/chronological/strategy_resolver.rb
@@ -10,20 +10,37 @@ module Chronological
       duration_until_a_relative_end:  [ :base_of_ending_offset,   :ending_offset,   :duration ]
     }
 
-    DEFAULT_FIELD_NAMES_FOR_STRATEGY_OPTIONS = {
-      starting_time:                  :started_at,
-      ending_time:                    :ended_at,
-      base_of_offset:                 :base_of_range_offset,
-      starting_offset:                :start_of_range_offset,
-      ending_offset:                  :end_of_range_offset,
-      base_of_starting_offset:        :base_of_range_starting_offset,
-      base_of_ending_offset:          :base_of_range_ending_offset,
-      duration:                       :duration_in_seconds,
-      absolute_start_date_field:      :started_on,
-      absolute_end_date_field:        :ended_on,
-      absolute_start_time_field:      :started_at,
-      absolute_end_time_field:        :ended_at
-    }
+    VALID_OPTIONS = [
+      :starting_time,
+      :ending_time,
+      :base_of_offset,
+      :starting_offset,
+      :ending_offset,
+      :base_of_starting_offset,
+      :base_of_ending_offset,
+      :duration,
+      :absolute_start_date_field,
+      :absolute_end_date_field,
+      :absolute_start_time_field,
+      :absolute_end_time_field,
+    ]
+
+    DEFAULT_FIELD_NAMES = [
+      :started_at,
+      :ended_at,
+      :base_of_range_offset,
+      :start_of_range_offset,
+      :end_of_range_offset,
+      :base_of_range_starting_offset,
+      :base_of_range_ending_offset,
+      :duration_in_seconds,
+      :started_on,
+      :ended_on,
+      :started_at,
+      :ended_at
+    ]
+
+    DEFAULT_FIELD_NAMES_FOR_OPTIONS = Hash[ VALID_OPTIONS.zip DEFAULT_FIELD_NAMES ]
 
     def self.resolve(options)
       strategy_name = resolve_strategy_name(options)
@@ -36,7 +53,7 @@ module Chronological
   private
     def self.parse(strategy_name, options)
       strategy_option_names   = STRATEGIES[strategy_name]
-      default_field_names     = DEFAULT_FIELD_NAMES_FOR_STRATEGY_OPTIONS.select do |option_name, default_field_name|
+      default_field_names     = DEFAULT_FIELD_NAMES_FOR_OPTIONS.select do |option_name, default_field_name|
                                   strategy_option_names.include? option_name
                                 end
       overridden_field_names  = options.select do |option_name, option_value|
