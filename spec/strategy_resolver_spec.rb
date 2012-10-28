a32d1cb00dbf7df7129b341356cffef2f7a3463b
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-28 15:22:32 -0500

    Add some `let`'s to the StrategyResolver spec

--- spec/strategy_resolver_spec.rb
+++ spec/strategy_resolver_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 describe Chronological::StrategyResolver do
   describe '.resolve' do
+    let(:resolved_strategy) { Chronological::StrategyResolver.resolve(options_to_resolve) }
+
     context 'when passed a strategy name' do
+      let(:options_to_resolve) { { :type => :absolute } }
+
       it 'resolves to the proper class' do
-        Chronological::StrategyResolver.resolve(:type => :absolute).should be_an Chronological::AbsoluteStrategy
+        resolved_strategy.should be_an Chronological::AbsoluteStrategy
       end
     end
   end
